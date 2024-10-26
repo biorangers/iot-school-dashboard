81dca238fd59ca4974318543951b786a7d15e5a2
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-10-26 19:12:42 +0300

    Enhance MusicCard component with radio station navigation and volume control

--- src/app/components/MusicCard.tsx
+++ src/app/components/MusicCard.tsx
@@ -1,16 +1,59 @@
 "use client";
 
-import { Paper, Typography, Button } from "@mui/material";
-import { useState } from "react";
+import { Paper, Typography, IconButton, Slider, Stack } from "@mui/material";
+import { useState, useEffect } from "react";
 
 import config from "@/config.json";
 
-// source: https://gist.github.com/Xifax/d330907d9ff0c059612b9c6161650b24
+import {
+  Pause,
+  PlayArrow,
+  SkipPrevious,
+  SkipNext,
+  VolumeDown,
+  VolumeUp,
+} from "@mui/icons-material";
 
 export default function MusicCard() {
   const [playing, setPlaying] = useState(false);
-  const [radio] = useState(config.radios[0]);
-  const [audio] = useState(new Audio(radio.url));
+  const [radio, setRadio] = useState(config.radios[0]);
+  const [audio] = useState(() => new Audio(config.radios[0].url)); // Initialize audio once
+  const [volume, setVolume] = useState(50);
+
+  useEffect(() => {
+    audio.volume = volume / 100; // Set initial volume
+  }, [audio, volume]);
+
+  function handleChange(event: Event, newValue: number | number[]) {
+    const newVolume = newValue as number;
+    setVolume(newVolume);
+    audio.volume = newVolume / 100; // Adjust volume directly
+  }
+
+  function changeRadio(direction: "next" | "previous") {
+    audio.pause();
+
+    let newRadio;
+    if (direction === "next") {
+      const nextRadioIndex = config.radios.indexOf(radio) + 1;
+      newRadio =
+        nextRadioIndex < config.radios.length
+          ? config.radios[nextRadioIndex]
+          : config.radios[0];
+    } else {
+      const previousRadioIndex = config.radios.indexOf(radio) - 1;
+      newRadio =
+        previousRadioIndex >= 0
+          ? config.radios[previousRadioIndex]
+          : config.radios[config.radios.length - 1];
+    }
+
+    audio.src = newRadio.url; // Update audio source
+    setRadio(newRadio);
+
+    audio.play();
+    setPlaying(true);
+  }
 
   function play() {
     if (playing) {
@@ -32,9 +75,32 @@ export default function MusicCard() {
       }}
     >
       <Typography variant="h6">{radio.name}</Typography>
-      <Button variant="contained" onClick={play}>
-        {playing ? "Stop" : "Play"}
-      </Button>
+      <div className="flex justify-between items-center max-h-8">
+        <IconButton onClick={() => changeRadio("previous")}>
+          <SkipPrevious />
+        </IconButton>
+        <IconButton onClick={play}>
+          {playing ? <Pause /> : <PlayArrow />}
+        </IconButton>
+        <IconButton onClick={() => changeRadio("next")}>
+          <SkipNext />
+        </IconButton>
+        <Stack
+          spacing={1}
+          direction="row"
+          sx={{ alignItems: "center", width: 200 }}
+        >
+          <VolumeDown />
+          <Slider
+            aria-label="Volume"
+            value={volume}
+            onChange={handleChange}
+            color="info"
+            valueLabelDisplay="auto"
+          />
+          <VolumeUp />
+        </Stack>
+      </div>
     </Paper>
   );
 }
