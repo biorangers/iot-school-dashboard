341d86a07a9a86e7275f6725382812e9a2b524e4
code-a-man <43219246+code-a-man@users.noreply.github.com>
2024-10-26 13:37:24 +0300

    WIP: make schedule card dynamic

--- src/app/components/ScheduleCard.tsx
+++ src/app/components/ScheduleCard.tsx
@@ -1,6 +1,35 @@
+"use client";
+
 import { Paper, Typography } from "@mui/material";
+import config from "@/config.json";
+
+import dayjs from "dayjs";
+import "dayjs/locale/tr";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import { useEffect, useState } from "react";
+import { Schedule } from "@/types/types";
 
 export default function ScheduleCard() {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.locale("tr");
+
+  const [weekDay, setWeekDay] = useState<number>(getWeekDay());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setWeekDay(getWeekDay());
+      setSchedule(config.schedule[weekDay] || []);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [weekDay]);
+
+  const [schedule, setSchedule] = useState<Schedule[]>(() => {
+    return config.schedule[weekDay] || [];
+  });
+
   return (
     <Paper
       sx={{
@@ -10,9 +39,17 @@ export default function ScheduleCard() {
         height: "100%",
       }}
     >
-      <Typography variant="h6">Ders Programı</Typography>
-      <Typography variant="body2">10:00 - 11:30 Algoritmalar</Typography>
-      <Typography variant="body2">13:00 - 14:30 Veri Yapıları</Typography>
+      <Typography variant="h6">Ders Programı {weekDay}</Typography>
+      {/* <Typography variant="body2">10:00 - 11:30 Algoritmalar</Typography>
+      <Typography variant="body2">13:00 - 14:30 Veri Yapıları</Typography> */}
+      {(schedule.length > 0 &&
+        schedule.map((item) => (
+          <Typography variant="body2" key={item.id}>
+            {item.time.start} - {item.time.end} {item.name}
+          </Typography>
+        ))) || <Typography variant="body2">Bugün ders yok</Typography>}
     </Paper>
   );
 }
+
+const getWeekDay = () => parseInt(dayjs().tz("Europe/Istanbul").format("d"));
